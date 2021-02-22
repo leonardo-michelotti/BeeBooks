b851dad1728f0683c2634f6e5e2684998b697a96
michelottilm <ldmichelotti@inf.ufsm.br>
2021-02-22 03:02:51 -0300

    dia 1

--- App.js
+++ App.js
@@ -1,21 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const LotsOfStyles = () => {
+    
+    return (
+      
+      <View style={styles.Container}>   
+
+        {/* <Image source={require('./logo.png')} /> */}
+
+          <Text style={styles.title}>BeeBook</Text>
+        
+            <View style={styles.CenterBox}>
+        
+              <View style={styles.PostContainer}>
+        
+                {/* <Input placeholder= "Email"/> */}
+              
+              </View> 
+              {/* Fim da PostContainer */}
+        
+            </View> 
+            {/*Fim da CenterBox*/}
+      
+      </View> 
+      //Fim da Containers
+    );
+};
 
 const styles = StyleSheet.create({
-  container: {
+
+    Container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#1C2025",
+  },
+  PostContainer: {
+    margin: 20,
+    padding: 20,
+  },
+  title: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 30,
+    flex: 0.2,
+  },
+  titleInterno: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  CenterBox: {
+    width: 300,
+    height: 300,
+    backgroundColor: '#FBC531',
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 50,
   },
 });
+
+export default LotsOfStyles;
